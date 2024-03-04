5b84012fdc1a2931c3907cd4bff7bd2b509076b6
natarajan pragadeesh <natrajanpragadeesh@gmail.com>
2024-03-04 14:05:37 +0400

    tsx warn fix

--- jest.config.js
+++ jest.config.js
@@ -3,16 +3,21 @@ const {defaults: tsjPreset} = require('ts-jest/presets');
 module.exports = {
   ...tsjPreset,
   preset: 'react-native',
-  globals: {
-    'ts-jest': {
-      babelConfig: true,
-    },
-  },
+  // globals: {
+  //   'ts-jest': {
+  //     babelConfig: true,
+  //   },
+  // },
   // moduleNameMapper: {
   //   '^@/(.*)$': '<rootDir>/$1',
   //   '^~/(.*)$': '<rootDir>/$1',
   //  '@react-navigation': '<rootDir>/__mocks__/@react-navigation.js',
   // },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {//the content you'd placed at "global"
+      babelConfig: true
+    }]
+  },
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
   transformIgnorePatterns: [
     '<rootDir>/node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|react-native-code-push)',
